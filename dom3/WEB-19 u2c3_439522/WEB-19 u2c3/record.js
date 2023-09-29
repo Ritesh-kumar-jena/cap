0b9e0daed2e3752c8a1c3506c71b9cbf9ce742ce
Ritesh-kumar-jena <jenariteshkumar85@gmail.com>
2023-09-29 12:29:54 +0530

    MASAI EMPLOYEE RECORDS

---
+++ dom3/WEB-19 u2c3_439522/WEB-19 u2c3/record.js
@@ -0,0 +1,49 @@
+// fill in javascript code here
+let myfrom=document.querySelector("form")
+let Name=document.getElementById("name")
+let ID=document.getElementById("employeeID")
+let Department=document.getElementById("department")
+let Exprience=document.getElementById("exp")
+let Email=document.getElementById("email")
+let Mobile=document.getElementById("mbl")
+let tbody=document.querySelector("tbody")
+let Alldata=[]
+myfrom.addEventListener("submit",function(e){
+    e.preventDefault()
+    let role;
+    if(Exprience.value > 5){
+        role="Senior"
+    }
+    else if(Exprience.value >= 2 && Exprience.value <= 5){
+        role="Junior"
+    }
+    else{
+        role="Fresher"
+    }
+    let data ={
+        name : Name.value ,
+         id :  ID.value ,
+         department :Department.value,
+         exprience : Exprience.value,
+         email : Email.value,
+         mobile :Mobile.value
+    }
+    Alldata.push(data)
+    let tr=document.createElement("tr")
+  let arr=[data.name,data.id,data.department,data.exprience,data.email,data.mobile,role]
+  arr.forEach((ele)=>{
+    let td=document.createElement("td")
+    td.innerText=ele
+    tr.append(td)
+  })
+tbody.append(tr)
+let delettd=document.createElement("td")
+let deletbtn=document.createElement("button")
+deletbtn.innerText="Delete"
+deletbtn.addEventListener("click",function(){
+tbody.removeChild(tr)
+})
+delettd.append(deletbtn)
+tr.append(delettd)
+
+})
