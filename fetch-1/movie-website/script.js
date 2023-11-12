05c8620b5b9224ef72744c8f2046d4d115a71d06
Ritesh-kumar-jena <jenariteshkumar85@gmail.com>
2023-11-12 08:19:51 +0530

    MOVIE APP I

--- fetch-1/movie-website/script.js
+++ fetch-1/movie-website/script.js
@@ -1,18 +1,51 @@
 let body=document.getElementById("body")
 let container=document.getElementById("container")
 let output="";
-const showMovies=async()=> {
-   
-    try{
-        let query=document.querySelector("#searchBox").value;
-        let url=`http://www.omdbapi.com/?i=tt3896198&apikey=e2ccd074&s=${query}`;
-        let res= await fetch(url)
-        let data=await res.json()
-        showdata(data.Search)
-    }catch (error){
-        console.error("There is a problem with your featch requst ",error.message)
-        };       
+let input=document.querySelector("#searchBox");
+let searchbar=document.getElementById("serchbar")
+let menu=document.getElementById("menu")
+
+async function  performance(query){
+  try{
+        
+    let url=`http://www.omdbapi.com/?i=tt3896198&apikey=e2ccd074&s=${query}`;
+    let res= await fetch(url)
+    let data=await res.json()
+    showdata(data.Search)
+}catch (error){
+    console.error("There is a problem with your featch requst ",error.message)
+    };       
 }
+
+function showMovies() {
+  let inputVlue=input.value;
+   performance(inputVlue)
+   menu.style.display="block"
+   searchbar.innerHTML=""
+   searchbar.style.display="none"
+   
+}
+
+//        DEBOUNCING use for  display the movies img and deatiles 
+
+function debounce(fun,delay){
+  let timer;
+  function inner(query){
+    clearTimeout(timer)
+    timer= setTimeout(()=>{
+    fun(query)
+    },delay)
+  }
+  return inner;
+}
+const debounceSearch=debounce(performance,500);
+input.addEventListener("input",()=>{
+  let inputVlue=input.value;
+   debounceSearch(inputVlue)
+   thortolsearch(inputVlue)
+})
+
+
 showdata=(data)=>{
   body.style.backgroundImage="url()"
    container.innerHTML=""
@@ -44,3 +77,56 @@ showdata=(data)=>{
     container.append(h1)
    }
 }
+
+   // This fatch requst use for Thorttling(i didn't use same fatch beacuse i want when i click the
+   //                                       search button my all thortlig data and that searchbar should vanish)
+
+async function  perform(query){
+  try{
+        
+    let url=`http://www.omdbapi.com/?i=tt3896198&apikey=e2ccd074&s=${query}`;
+    let res= await fetch(url)
+    let data=await res.json()
+    showresult(data.Search)
+}catch (error){
+    console.error("There is a problem with your featch requst ",error.message)
+    };       
+}
+
+//    THROTTLING use for showing movies title in searchbar
+
+function thortling(func,dely){
+let thortl=false
+function iner(query){
+  if(thortl==false){
+    thortl=true
+    func(query)
+    setTimeout(()=>{
+      thortl=false
+    },dely)
+  }
+}
+return iner
+}
+const thortolsearch=thortling(perform,200)
+
+showresult=(data)=>{
+  searchbar.innerHTML=""
+  menu.style.display="none"
+     searchbar.style.display="block"
+  if(data!==undefined){
+    data.forEach(function(ele){
+      let title=document.createElement("p")
+      title.innerText=ele.Title
+      searchbar.append(title)
+     })
+    
+  }
+    else{
+     menu.style.display="block"
+   searchbar.innerHTML=""
+   searchbar.style.display="none"
+    }
+    
+}
+
